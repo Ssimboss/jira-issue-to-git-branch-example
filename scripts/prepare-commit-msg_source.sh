54a302cb8a523003dd382425da3a7a7fdb1b2dca
Andrei Simvolokov <simbos@yandex.ru>
2026-01-10 20:00:38 +0000

    prepare-commit-msg_source.sh fix

--- scripts/prepare-commit-msg_source.sh
+++ scripts/prepare-commit-msg_source.sh
@@ -7,7 +7,7 @@ MESSAGE=`cat "$1"`
 
 if [[ $BRANCH == *"$TASK_PREFIX"* ]]; then
     TASK_NAME="$(echo "$BRANCH" | sed -E "s/.*\/(${TASK_PREFIX}[0-9]+).*/\1/")"
-    if [[ MESSAGE == $TASK_NAME* ]] || [[ MESSAGE == Merge* ]] || [[ -z "$TASK_NAME" ]]
+    if [[ $MESSAGE == $TASK_NAME* ]] || [[ $MESSAGE == Merge* ]] || [[ -z "$TASK_NAME" ]]
     then
         echo "$MESSAGE" > "$1"
     else
